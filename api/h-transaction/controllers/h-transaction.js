7fd0d528efdc43c6775bc7e6a1505fde69a59a64
rezalimena <rezaliem07@gmail.com>
2021-05-02 14:03:45 +0700

    fix bug

--- api/h-transaction/controllers/h-transaction.js
+++ api/h-transaction/controllers/h-transaction.js
@@ -7,6 +7,7 @@
 
 module.exports = {
   async salary(ctx) {
+    console.log('object');
 
     const {id} = ctx.params
     const {start} = ctx.params
@@ -15,23 +16,26 @@ module.exports = {
     const therapist = await strapi.services["therapist"].findOne({ id });
 
     const comission = therapist.comission
-
     const htransaction = await strapi.services["h-transaction"].find({ date_gte:start, date_lte:end });
+    
     const result = []
     for (let index = 0; index < htransaction.length; index++) {
       const idHtrans = htransaction[index].id;
-      const singleTrans = {}
-      singleTrans.htrans = htransaction[index]
       const dtransaction = await strapi.services["d-transaction"].find({ 
         h_transaction : idHtrans, 
         therapist:id 
       });
-      singleTrans.detail = dtransaction
-      result.push(singleTrans)
+      if(dtransaction.length > 0)
+      {
+        const singleTrans = {}
+        singleTrans.htrans = htransaction[index]
+        singleTrans.detail = dtransaction
+        result.push(singleTrans)
+      }
     }
-
     for (let index = 0; index < result.length; index++) {
       const transaction = result[index];
+
       const omset = transaction.detail.map(detail => detail.type.base_price).reduce((prev, curr) => prev + curr)
       const commission =transaction.detail.map(detail => detail.type.base_price * comission / 100
       ).reduce((prev, curr) => prev + curr)

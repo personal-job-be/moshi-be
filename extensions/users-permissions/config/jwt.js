e1e56ee89410a6fd8d5e16fa597dd2076d084724
rezalimena <rezaliem07@gmail.com>
2021-05-18 22:43:30 +0700

    add expired

--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -1,3 +1,6 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '43ea2130-98a6-466c-8172-2e8ed5ee63de'
+  jwtSecret: process.env.JWT_SECRET || '43ea2130-98a6-466c-8172-2e8ed5ee63de',
+  jwt: {
+    expiresIn: "30d",
+  }
 };
